7725f56ca730f86ca7573f46d0b4b52ada1bde8a
ArturJS <nizamutdin.art@gmail.com>
2018-12-04 20:46:37 +0400

    Added unit test for 'extractIdsToRemove' method

--- local_modules/apollo-client-resolvers/__tests__/utils.test.js
+++ local_modules/apollo-client-resolvers/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-import { getAllByType } from '../utils';
+import { getAllByType, extractIdsToRemove } from '../utils';
 
 describe('Check utils.js', () => {
   describe('Check `getAllByType` method', () => {
@@ -14,6 +14,100 @@ describe('Check utils.js', () => {
       const result = getAllByType({ data: testData, type: 'Card' });
 
       expect(result).toEqual(['Card:1-value', 'Card:2-value', 'Card:3-value']);
+    });
+  });
+
+  describe('Check `extractIdsToRemove` method', () => {
+    it('should work for array of objects', () => {
+      const persistedItems = [
+        {
+          id: 1,
+          __typename: 'Card'
+        },
+        {
+          id: 1,
+          __typename: 'Desk'
+        }
+      ];
+      const prevData = {
+        items: [
+          ...persistedItems,
+          {
+            id: 2,
+            __typename: 'Card'
+          },
+          {
+            id: 3,
+            __typename: 'Desk'
+          }
+        ]
+      };
+      const nextData = {
+        items: [
+          {
+            id: 10,
+            __typename: 'Card'
+          },
+          ...persistedItems,
+          {
+            id: 20,
+            __typename: 'Desk'
+          }
+        ]
+      };
+      const result = extractIdsToRemove({ prevData, nextData });
+
+      expect(result).toEqual(['Card:2', 'Desk:3']);
+    });
+
+    it('should work for nested objects and arrays', () => {
+      const prevData = {
+        item: {
+          id: 1,
+          __typename: 'Card'
+        },
+        data: [
+          {
+            id: 2,
+            __typename: 'Card'
+          },
+          [
+            {
+              id: 3,
+              __typename: 'Card'
+            }
+          ],
+          'Extra data'
+        ]
+      };
+      const nextData = {
+        item: {
+          id: 1,
+          __typename: 'Card'
+        },
+        some: {
+          deep: {
+            nested: {
+              data: [
+                {
+                  id: 3,
+                  __typename: 'Card',
+
+                  children: [
+                    {
+                      id: 4,
+                      __typename: 'Card'
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      };
+      const result = extractIdsToRemove({ prevData, nextData });
+
+      expect(result).toEqual(['Card:2']);
     });
   });
 });
